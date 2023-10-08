2b733b75bd15a2850b8a514da16c18d47243b71c
waheilee <waheilee@gmail.com>
2023-10-08 13:26:12 +0800

    btiplus

---
+++ application/api/controller/Js.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\api\controller;
+
+
+use socket\QuerySocket;
+use think\Controller;
+use think\Request;
+
+class Js extends Controller
+{
+
+    public function refresh(Request $request)
+    {
+        $token = $request->get('token');
+        $roleId = intval($this->decry($token));
+        $socket = new QuerySocket();
+        $m = $socket->DSQueryRoleBalance($roleId);
+        $gameMoney = $m['iGameWealth'] ?? 0;
+        $balance = bcdiv($gameMoney, bl, 3);
+        return floor($balance * 100) / 100;
+    }
+
+    //解密
+    private function decry($str, $key = 'btiplus')
+    {
+        return think_decrypt($str, $key);
+    }
+}
