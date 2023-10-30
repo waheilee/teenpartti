5aa67487ce32cf3494e5b313f8c70ec1d1ad97c3
waheilee <waheilee@gmail.com>
2023-10-30 13:59:41 +0800

    feat:u

--- application/model/GameOCDB.php
+++ application/model/GameOCDB.php
@@ -1503,7 +1503,8 @@ class GameOCDB extends BaseModel
             $where .= " AND OperateType=" . $operatortype;
         }
         if (!empty(session('business_LoginAccount'))){
-            $where .= " AND checkUser=" . session('business_LoginAccount');
+            $businessAccount = session('business_LoginAccount');
+            $where .= " AND checkUser=" . "$businessAccount";
         }
         $result = $this->GetPage($where, 'ID DESC');
         if (empty($where)) $where = "status=1";
