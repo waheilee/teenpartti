542697718c5c3a0fe40ef9c5da0a8fc303967bdc
waheilee <waheilee@gmail.com>
2023-12-11 11:32:13 +0800

    渠道后台增加打码任务管理

--- application/merchant/controller/Ranke.php
+++ application/merchant/controller/Ranke.php
@@ -5,6 +5,7 @@ namespace app\merchant\controller;
 
 use app\model\AccountDB;
 use app\model\UserDB;
+use socket\QuerySocket;
 use think\response\Json;
 
 
@@ -129,4 +130,133 @@ class Ranke extends Main
         return $this->fetch();
     }
 
+    ///打码任务排行
+    public function wageTaskList()
+    {
+        switch (input('Action')) {
+            case 'list':
+                $db = new UserDB();
+                $data = $db->GetGoldRanklist();
+                $socket = new QuerySocket();
+                foreach ($data['list'] as $k => &$v) {
+                    $userbanlance = $socket->DSQueryRoleBalance($v['AccountID']);
+                    $v['CashAble'] = 0;
+                    if (!empty($userbanlance)) {
+                        $v['iGameWealth'] = $userbanlance['iGameWealth'];
+                        $v['iFreezonMoney'] = $userbanlance['iFreezonMoney'];
+                        $v['iNeedWaged'] = $userbanlance['iNeedWaged'];
+                        $v['iCurWaged'] = $userbanlance['iCurWaged'];
+                        $v['CashAble'] = bcsub($v['iGameWealth'], $v['iFreezonMoney'], 2);
+                    } else {
+                        $v['iGameWealth'] = 0;
+                        $v['iFreezonMoney'] = 0;
+                        $v['iNeedWaged'] = 0;
+                        $v['iCurWaged'] = 0;
+                    }
+                    ConVerMoney($v['CashAble']);
+                    ConVerMoney($v['TotalWeath']);
+                    ConVerMoney($v['iGameWealth']);
+                    ConVerMoney($v['iFreezonMoney']);
+                    ConVerMoney($v['iNeedWaged']); //打码任务
+                    ConVerMoney($v['iCurWaged']);//当前打码量
+                    $number = number_format($v['percentage'],4,'.','');
+                    $v['percentage'] = bcmul($number, 100, 2) . '%';
+
+//                    if ($v['iCurWaged'] == 0 || $v['iNeedWaged'] == 0){
+//                        $v['percentage'] = '0%';
+//                    }else{
+//                        $v['percentage'] = bcmul(bcdiv( $v['iCurWaged'] , $v['iNeedWaged'],6),100,2).'%';
+//                    }
+                    unset($v);
+                }
+                return $this->apiJson($data);
+            case 'exec':
+                //权限验证
+                $auth_ids = $this->getAuthIds();
+                if (!in_array(10008, $auth_ids)) {
+                    return $this->apiJson(["code" => 1, "msg" => "没有权限"]);
+                }
+                $db = new UserDB();
+                $result = $db->GetGoldRanklist();
+
+                $outAll = input('outall', false);
+                if ((int)input('exec', 0) == 0) {
+                    if ($result['count'] == 0) {
+                        $result = ["count" => 0, "code" => 1, 'msg' => lang("没有找到任何数据,换个姿势再试试?")];
+                    }
+                    if ($result['count'] >= 5000 && $outAll == false) {
+                        $result = ["code" => 2, 'msg' => lang("数据超过5000行是否全部导出?<br>数据越多速度越慢<br>当前数据一共有") . $result['count'] . lang("行")];
+                    }
+                    unset($result['list']);
+                    return $this->apiJson($result);
+                }
+
+                //导出表格
+                if ((int)input('exec', 0) == 1 && $outAll = true) {
+                    $header_types = [
+                        lang('玩家ID') => 'integer',
+                        lang('渠道ID') => 'string',
+                        lang('注册时间') => 'string',
+                        lang('携带金币') => 'string',
+                        lang('总和') => 'string',
+                        lang('总充值') => 'string',
+                        lang('总提现') => 'string',
+                        lang('冻结金额') => 'string',
+                        lang('可提金额') => 'string',
+                        lang('当前完成打码') => 'string',
+                        lang('打码任务') => 'string',
+                    ];
+                    $filename = '金币排行-' . date('YmdHis');
+                    $rows =& $result['list'];
+                    $writer = $this->GetExcel($filename, $header_types, $rows, true);
+                    $socket = new QuerySocket();
+                    foreach ($rows as $index => &$row) {
+                        $v = &$row;
+                        $userbanlance = $socket->DSQueryRoleBalance($v['AccountID']);
+                        $v['CashAble'] = 0;
+                        if (!empty($userbanlance)) {
+                            $v['iGameWealth'] = $userbanlance['iGameWealth'];
+                            $v['iFreezonMoney'] = $userbanlance['iFreezonMoney'];
+                            $v['iNeedWaged'] = $userbanlance['iNeedWaged'];
+                            $v['iCurWaged'] = $userbanlance['iCurWaged'];
+                            $v['CashAble'] = bcsub($v['iGameWealth'], $v['iFreezonMoney'], 2);
+                        } else {
+                            $v['iGameWealth'] = 0;
+                            $v['iFreezonMoney'] = 0;
+                            $v['iNeedWaged'] = 0;
+                            $v['iCurWaged'];
+                        }
+                        ConVerMoney($v['CashAble']);
+                        ConVerMoney($v['TotalWeath']);
+                        ConVerMoney($v['iGameWealth']);
+                        ConVerMoney($v['iFreezonMoney']);
+                        ConVerMoney($v['iNeedWaged']);
+                        ConVerMoney($v['iCurWaged']);
+                        if ($v['iNeedWaged'] <= $v['iCurWaged']) {
+                            $v['iCurWaged'] = $v['iNeedWaged'] = '0.00';
+                        }
+                        $item = [
+                            $row['AccountID'],
+                            $row['OperatorId'],
+                            $row['RegisterTime'],
+                            $row['iGameWealth'],
+                            $row['iGameWealth'],
+                            $row['TotalDeposit'],
+                            $row['TotalRollOut'],
+                            $row['iFreezonMoney'],
+                            $row['CashAble'],
+                            $row['iCurWaged'],
+                            $row['iNeedWaged'],
+                        ];
+                        $writer->writeSheetRow('sheet1', $item, ['height' => 16, 'halign' => 'center',]);
+                        unset($rows[$index]);
+                    }
+                    unset($row, $item);
+                    $writer->writeToStdOut();
+                    exit();
+                }
+        }
+
+        return $this->fetch();
+    }
 }
