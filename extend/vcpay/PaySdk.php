1b803397029d09ae5d185bb7150fc0dbd7b9e558
waheilee <waheilee@gmail.com>
2023-12-19 21:01:59 +0800

    新支付

--- extend/vcpay/PaySdk.php
+++ extend/vcpay/PaySdk.php
@@ -11,139 +11,81 @@ namespace vcpay;
 class PaySdk
 {
 
-    private $api_url = '';
-    private $notify_url = '';
-    private $merchant = '';
-    private $appid = '';
-    private $secret = '';
 
-
-    public function __construct()
+    public function payout($OrderNo, $order, $config = [])
     {
-        $this->api_url = '';
-        $this->merchant = '';
-        $this->appid = '';
-        $this->secret = '';
-    }
-
-
-
-
-
-    public function payout($OrderNo,$order,$config=[]){
-        if (empty($config)) {
-            return array('status'=>FALSE, 'message' => 'Missing parameter');
+        if (!isset($order['PayWayType'])){
+            $result['system_ref'] = '';
+            $result['status'] = false;
+            $result['message'] = '出款类型有误，请检查后在提交';
+            return $result;
         }
-        if (isset($config['merchant']) && !empty($config['merchant'])) {
-            $this->merchant = $config['merchant'];
-        }
-        if (isset($config['appid']) && !empty($config['appid'])) {
-            $this->appid = $config['appid'];
-        }
-        if (isset($config['secret']) && !empty($config['secret'])) {
-            $this->secret = $config['secret'];
+
+        if ($order['PayWayType'] == 6) {
+            //PHONE
+            $identityType = 'PHONE';
+            $identity = $order['CardNo'];
+        } elseif ($order['PayWayType'] == 7) {
+            //EMAIL
+            $identityType = 'EMAIL';
+            $identity = $order['CardNo'];
         } else {
-            $this->secret = '';
+            //CPF
+            $identityType = 'CPF';//收款账户类型
+            $identity = $order['Province'];
         }
-        if (isset($config['apiurl']) && !empty($config['apiurl'])) {
-            $this->api_url = $config['apiurl'];
-        } else {
-            $this->api_url = '';
-        }
-        if (!isset($config['notify_url']) || empty($config['notify_url'])) {
-            return array('status'=>FALSE, 'message' => 'Missing notify_url');
-        }
-    
 
-        $merchant    = $this->merchant;
-        $appid       = $this->appid;
-        $orderId      = trim($OrderNo);
-        $amount       = sprintf('%.2f',$order['RealMoney']);
-        $notify_url   = trim($config['notify_url']);
-        $mobile      = rand(6,9).rand(100000000,999999999);
-        $username    = chr(rand(65,90)).chr(rand(97,122)).chr(rand(97,122)).chr(rand(97,122)).chr(rand(97,122)).chr(rand(97,122));
-        $email       = $mobile.'@gmail.com';
-        $RealName    = trim($order['RealName']);
-        $timestamp   = time();
-
-        if(strlen($order['City']) ==13){
-            $order['City'] = substr($order['City'], 3);
-        }
-        if(strlen($order['City']) ==12){
-            $order['City'] = substr($order['City'], 2);
-        }
-        $postdata = [
-            'app_id'        =>$appid,
-            'nonce_str'     =>rand(10000000,99999999).sprintf('%.0f', floatval(explode(' ', microtime())[0]) * 1000),
-            'trade_type'    =>$config['code'],
-            'order_amount'  =>(int)$amount*100,
-            'out_trade_no'  =>$orderId,
-            'notify_url'    =>$notify_url,
-            'bank_code'     =>'000000',
-            'bank_owner'    =>trim($order['RealName']),
-            'bank_account'  =>(int)trim($order['CardNo']),
-            'identity_type' =>'PHONE',
-            'identity'      =>trim($order['CardNo']),
+        $appId = $config['app_id'] ?? '';
+        $orderId = trim($OrderNo);
+        $amount = sprintf('%.2f', $order['RealMoney']);
+        $notifyUrl = $config['notify_url'] ?? '';
+        $apiUrl = $config['api_url'] ?? '';
+        $secret = $config['secret'] ?? '';
+        $postData = [
+            'app_id' => $appId,
+            'nonce_str' => rand(10000000, 99999999) . sprintf('%.0f', floatval(explode(' ', microtime())[0]) * 1000),
+            'trade_type' => $config['code'],
+            'order_amount' => (int)$amount * 100,
+            'out_trade_no' => $orderId,
+            'notify_url' => $notifyUrl,
+            'bank_code' => '000000',
+            'bank_owner' => trim($order['RealName']),
+            'bank_account' => $order['CardNo'],
+            'identity_type' => $identityType,
+            'identity' => $identity,
         ];
-        
-        $postdata['sign'] = $this->createSign($postdata,$this->secret);
+
+        $postData['sign'] = $this->createSign($postData, $secret);
 
         $header = [
             'Content-Type:application/json;charset=UTF-8',
         ];
 
-        $result =$this->curl_post_content($this->api_url.'/wd/save',json_encode($postdata), $header);
- 
-        save_log('vcpay','post:'.json_encode($postdata).',output:'.$result);
+        $result = $this->curl_post_content($apiUrl . '/wd/save', json_encode($postData), $header);
+
+        save_log('vcpay', 'post:' . json_encode($postData) . ',output:' . $result);
         $res = json_decode($result, true);
-        $result =['system_ref'=>'','message'=>''];
+        $result = ['system_ref' => '', 'message' => ''];
         if ($res) {
-            if($res['code'] == '200'){
+            if ($res['code'] == '200') {
                 $result['system_ref'] = '';
                 $result['status'] = true;
                 $result['message'] = 'success';
-            }
-            else
-            {
-                $result['message'] =$res['msg'];
+            } else {
+                $result['message'] = $res['msg'];
                 $result['status'] = false;
             }
         } else {
             $result['system_ref'] = '';
-            $result['status'] = true;
-            $result['message'] = 'success';
+            $result['status'] = false;
+            $result['message'] = '订单同步回调失败';
         }
         return $result;
     }
 
 
-    ///查询订单
-    public function queryorder($config, $orderid)
-    {
-        if (!empty($orderid)) {
-            if ($config) {
-                $this->appid = $config['appid'];
-                $this->secret = $config['secret'];
-            }
-            $postdata = [
-                'memberid' => $this->appid,
-                'out_trade_no' => $orderid
-            ];
-            $md5sing = $this->createSign($this->secret, $postdata);
-            $postdata['sign'] = $md5sing;
-
-            $requestHttpDate = http_build_query($postdata);
-            $result = $this->httpRequestDataTest($this->api_url . '/Payment_Dfpay_query.html', $requestHttpDate);    //发送http的post请求
-            save_log('tgpay','query order post:'.json_encode($postdata).',output:'.$result);
-            $res = json_decode($result, true);
-            return $res;
-        }
-    }
-
-
-
     //签名函数
-    private function createSign($data,$Md5key)
+    private function createSign($data, $md5key)
     {
         ksort($data);
         $md5str = '';
@@ -152,12 +94,12 @@ class PaySdk
                 $md5str = $md5str . $key . '=' . $val . '&';
             }
         }
-        $str =$md5str . 'key=' . $Md5key;
+        $str = $md5str . 'key=' . $md5key;
         return strtoupper(md5($str));
     }
 
     //http请求函数
-   private function curl_post_content($url, $data = null, $header = [])
+    private function curl_post_content($url, $data = null, $header = [])
     {
         $ch = curl_init();
         if (substr_count($url, 'https://') > 0) {
