9d692fe4b36ddd1bd7517c900fc6692255510e4c
waheilee <waheilee@gmail.com>
2024-01-11 14:34:45 +0800

    brpay

--- application/admin/controller/import/ImportUser.php
+++ application/admin/controller/import/ImportUser.php
@@ -3,6 +3,8 @@
 namespace app\admin\controller\import;
 
 use app\admin\controller\Main;
+use app\model\AccountDB;
+use think\Env;
 use think\Loader;
 use PHPExcel;
 
@@ -17,7 +19,8 @@ class ImportUser extends Main
 
     function inserExcel()
     {
-//        Loader::import('PHPExcel.Classes.PHPExcel');
+
+//        Loader::import('PHPExcel.PHPExcel');
 //        Loader::import('PHPExcel.Classes.PHPExcel.IOFactory.PHPExcel_IOFactory');
 //        Loader::import('PHPExcel.Classes.PHPExcel.Reader.Excel5');
         //获取表单上传文件
@@ -28,7 +31,7 @@ class ImportUser extends Main
         if ($info) {
 //            echo $info->getFilename();
             $excel = new PHPExcel();
-            dump($excel);die();
+
             $exclePath = $info->getSaveName();  //获取文件名
             $file_name = ROOT_PATH . 'public' . DS . 'uploads' . DS . $exclePath;   //上传文件的地址
             $objReader = \PHPExcel_IOFactory::createReader('Excel2007');
@@ -36,16 +39,57 @@ class ImportUser extends Main
             echo "<pre>";
             $excel_array = $obj_PHPExcel->getsheet(0)->toArray();   //转换为数组格式
             array_shift($excel_array);  //删除第一个数组(标题);
-            dump($excel_array);die();
-            $city = [];
+
+            $errorInfo = [];
+            $success_num = 0;
+            $error_num=0;
             foreach ($excel_array as $k => $v) {
-                $city[$k]['Id'] = $v[0];
-                $city[$k]['code'] = $v[1];
-                $city[$k]['path'] = $v[2];
-                $city[$k]['pcode'] = $v[3];
-                $city[$k]['name'] = $v[4];
+
+                $regIpIn = (string)$v[0];
+                $regPhone = (int)$v[1];
+                $password = (string)$v[2];
+                $inviteCode = (string)$v[3];
+
+                $userAccountDB = new AccountDB();
+                $sqlExec = "exec P_Test_Accounts_Bind_Insert '$regIpIn','$regPhone','$password', '$inviteCode'";
+//                $sql = "{CALL [dbo].[P_Test_Accounts_Bind_Insert] (?, ?, ?, ?)}";
+//                $data = [
+//                    $regIpIn,
+//                    $regPhone,
+//                    $password,
+//                    $inviteCode
+//                ];
+//                $res = $userAccountDB->getTableEXEC($sql,$data);
+                $res = $userAccountDB->getTableQuery($sqlExec);
+                $flattenedArray = call_user_func_array('array_merge', $res);
+
+                if ($flattenedArray[0]['info'] == 1){
+                    $errorInfo[] = [
+                        '电话号码' => $regPhone,
+                        '消息' => 'ip重复',
+                    ];
+                    $error_num +=1;
+                }elseif($flattenedArray[0]['info'] == 2){
+                    $errorInfo[] = [
+                        '电话号码' => $regPhone,
+                        '消息' => '电话号码重复',
+                    ];
+                    $error_num +=1;
+                }elseif($flattenedArray[0]['info'] == 3){
+                    $errorInfo[] = [
+                        '电话号码' => $regPhone,
+                        '消息' => '用户名重复',
+                    ];
+                    $error_num +=1;
+                }else{
+                    $success_num+= 1;
+                }
             }
-            Db::name('city')->insertAll($city); //批量插入数据
+dump('成功'.$success_num);
+dump('失败'.$error_num);
+print_r($errorInfo);
+//            $this->apiReturn(0, $errorInfo, '操作成功。成功：' . $success_num . ',失败：' . $error_num);
+
         } else {
             echo $file->getError();
         }
