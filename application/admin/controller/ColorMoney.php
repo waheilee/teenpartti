e343c3c79d2be5c30828a04f1ca4b6dea307aef2
waheilee <waheilee@gmail.com>
2024-01-22 17:48:30 +0800

    不打码彩金设置

--- application/admin/controller/ColorMoney.php
+++ application/admin/controller/ColorMoney.php
@@ -4,10 +4,63 @@ namespace app\admin\controller;
 
 use app\admin\controller\Export\AgentWaterDailyExport;
 use app\admin\controller\Export\ColorMoneyLogExport;
+use app\common\GameLog;
 use app\model\DataChangelogsDB;
+use app\model\GameOCDB;
 
 class ColorMoney extends Main
 {
+
+    /**
+     * 彩金设置
+     * @return mixed
+     */
+    public function setColorMoney()
+    {
+        $roleId = $this->request->param('roleid');
+        $amount = $this->request->param('amount');
+        $type = $this->request->param('type');
+
+        $sendDm = $amount * bl;
+        $key = 0;
+        if ($type == 1){
+            $key = 10213; //增加彩金
+        }elseif ($type == 2){
+            $key = 10215; //增加首充彩金
+        }elseif ($type == 3){
+            $key = 10211; //不打码彩金
+        }elseif ($type == 4){
+            $key = 10210; //不打码首充彩金
+        }
+        $data = $this->sendGameMessage('CMD_MD_GM_SET_JOB', [$roleId, $key, $sendDm], "DC", 'returnComm');
+        if ($data['iResult'] == 1) {
+            if ($type == 1) {
+                $comment = '增加玩家彩金：' . $amount;
+            } elseif($type == 2) {
+                $comment = '增加玩家首冲彩金:' . $amount;
+            }elseif($type == 3) {
+                $comment = '增加玩家不打码彩金:' . $amount;
+            }elseif($type == 4) {
+                $comment = '增加玩家不打码首充彩金:' . $amount;
+            }else{
+                $comment = '无效彩金增加:' . $amount;
+            }
+            $db = new GameOCDB();
+            $db->setTable('T_PlayerComment')->Insert([
+                'roleid' => $roleId,
+                'adminid' => session('userid'),
+                'type' => 2,
+                'opt_time' => date('Y-m-d H:i:s'),
+                'comment' => $comment
+            ]);
+
+            GameLog::logData(__METHOD__, [$roleId, $amount, $type], 1, $comment);
+            return $this->apiReturn(0, '', '操作成功');
+        } else {
+            GameLog::logData(__METHOD__, [$roleId, $amount, $type], 0, '操作失败');
+            return $this->apiReturn(1, '', '操作失败');
+        }
+    }
     public function colorMoneyCoinLog()
     {
         if ($this->request->isAjax()) {
--- application/admin/controller/Player.php
+++ application/admin/controller/Player.php
@@ -5164,44 +5164,7 @@ class Player extends Main
         }
     }
 
-    public function setColorMoney()
-    {
-        $roleId = $this->request->param('roleid');
-        $amount = $this->request->param('amount');
-        $type = $this->request->param('type');
 
-        $sendDm = $amount * bl;
-        $key = 0;
-        if ($type == 1){
-            $key = 10213; //增加彩金
-        }elseif ($type == 2){
-            $key = 10215; //增加首充彩金
-        }
-        $data = $this->sendGameMessage('CMD_MD_GM_SET_JOB', [$roleId, $key, $sendDm], "DC", 'returnComm');
-        if ($data['iResult'] == 1) {
-            if ($type == 1) {
-                $comment = '增加玩家彩金：' . $amount;
-            } elseif($type == 2) {
-                $comment = '增加玩家首冲彩金:' . $amount;
-            }else{
-                $comment = '无效彩金增加:' . $amount;
-            }
-            $db = new GameOCDB();
-            $db->setTable('T_PlayerComment')->Insert([
-                'roleid' => $roleId,
-                'adminid' => session('userid'),
-                'type' => 2,
-                'opt_time' => date('Y-m-d H:i:s'),
-                'comment' => $comment
-            ]);
-
-            GameLog::logData(__METHOD__, [$roleId, $amount, $type], 1, $comment);
-            return $this->apiReturn(0, '', '操作成功');
-        } else {
-            GameLog::logData(__METHOD__, [$roleId, $amount, $type], 0, '操作失败');
-            return $this->apiReturn(1, '', '操作失败');
-        }
-    }
 
 
 
