86d4af581398fc52023e2363169e1ba047d2c0c2
waheilee <waheilee@gmail.com>
2024-03-06 15:28:03 +0800

    feat:u

--- application/merchant/controller/Channel.php
+++ application/merchant/controller/Channel.php
@@ -917,24 +917,27 @@ class Channel extends Main
                 $other['HistoryCoin'] = FormatMoney($other['HistoryCoin']);
 
                 $other['Profit'] = bcsub($other['TotalRecharge'], $other['TotalDrawMoney'], 2);
+                $proxyChannel = (new GameOCDB())->getTableObject('T_ProxyChannelConfig')
+                    ->where('OperatorId',session('merchant_OperatorId'))
+                    ->column('ProxyChannelId');
+                $proxyChannelId = [];
+                if (!empty($proxyChannel)) {
+                    $proxyChannelId = array_flip($proxyChannel);
+                }
+
                 $other['FirstDepositMoney'] = (new AccountDB())->getTableObject('T_Accounts')
                     ->alias('a')
                     ->join('[CD_DataChangelogsDB].[dbo].[T_UserTransactionLogs](NOLOCK) b','b.RoleID=a.AccountID','left')
-                    ->where('a.OperatorId',session('merchant_OperatorId'))
-                    ->whereTime('b.AddTime','>=',$start.' 00:00:00')
-                    ->whereTime('b.AddTime','<=',$end.' 23:59:59')
+                    ->whereIn('a.ProxyChannelId',$proxyChannelId)
+                    ->where(function($q) use($start,$end){
+                        if (!empty($start) && !empty($end)){
+                            $q->where('b.AddTime','between',[$start.' 00:00:00',$end.' 23:59:59']);
+                        }
+                    })
                     ->where('b.ChangeType',5)
                     ->where('b.IfFirstCharge',1)
                     ->sum('b.TransMoney')?:0;
-//                $other['FirstDepositMoney'] = (new \app\model\DataChangelogsDB())
-//                    ->getTableObject('T_UserTransactionLogs')->alias('a')
-//                    ->join('[CD_Account].[dbo].[T_Accounts](NOLOCK) b', 'b.AccountID=a.RoleID', 'left')
-//                    ->where('b.OperatorId',session('merchant_OperatorId'))
-//                    ->whereTime('a.AddTime','>=',$start.' 00:00:00')
-//                    ->whereTime('a.AddTime','<=',$end.' 23:59:59')
-//                    ->where('a.ChangeType',5)
-//                    ->where('a.IfFirstCharge',1)
-//                    ->sum('TransMoney')?:0;
+
             }
             return $this->apiReturn(0, $data['data'], 'success', $data['total'], $other);
         }
